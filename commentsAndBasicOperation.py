dde85db8a63875e5a9eb06df92db8ff716f96313
zamirul <javshazib@gmail.com>
2026-04-02 07:04:49 +0600

    ceomment and basic operation learn done

---
+++ commentsAndBasicOperation.py
@@ -0,0 +1,44 @@
+# this is comments and basic operation
+
+# a= int(input("enter a number:")) # this      is input function and
+
+
+# python arithmetic operator 
+# addition
+# multplication 
+# division
+# subtraction
+# modulus
+# exponentiation
+# floor division
+
+a= 15
+b=2
+
+print(a+b) # addition 17
+print(a*b) # multiplication 30
+print(a/b) # division 7.5
+print(a-b) # subtraction 13
+print(a%b) # modulus 1
+print(a**b) # exponentiation 225
+print(a//b) # floor division 7
+
+
+# python comparison operation 
+# equal to ==
+# not equal to !=
+# greater than >
+# less than <
+# greater than or equal to >=
+# less than or equal to <=
+# boolean value 
+
+print("a==b:", a==b) # equal to False
+print("a!=b:", a!=b) # not equal to True
+print("a>b:", a>b) # greater than True
+print("a<b:", a<b) # less than False
+print("a>=b:", a>=b) # greater than or equal to True
+print("a<=b:", a<=b) # less than or equal to False
+
+
+
