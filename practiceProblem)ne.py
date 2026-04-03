4cc1b59830208514879c2eca7b733b321a14a8b6
zamirul <javshazib@gmail.com>
2026-04-03 14:58:10 +0600

    practice problem solve

---
+++ practiceProblem)ne.py
@@ -0,0 +1,8 @@
+a= int(input("enter the length of the rectangle: "))
+b= int(input("enter the breadth of the rectangle: "))
+
+if a== b:
+    print("the  is a square")
+else:
+    print("the  is a rectangle")
+
